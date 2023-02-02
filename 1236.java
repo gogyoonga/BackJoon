2e2336fa74b1f8b1c30aad3b7462fae88da8d77a
GoGyoonGa <100255375+gogyoonga@users.noreply.github.com>
2023-02-02 14:27:09 +0900

    Update 1236.java

--- 1236.java
+++ 1236.java
@@ -3,63 +3,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+
         String s = bf.readLine();
         StringTokenizer st = new StringTokenizer(s);
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        String []arr = new String[n];
-        int x=0; //추가할 경비원 수
-
-
-        for (int i=0; i<n; i++){
-            // 행
-            boolean status = false; //경비 없음 디폴트
-            arr[i] = bf.readLine();
-            for(int j=0; j<m; j++){
-                // 열
-                if (arr[i].charAt(j) == 'X'){
-                    status = true; //경비 있으면 상태변경
-                    break;
-                }
-                if(j==m-1){
-                    //열 끝까지 경비가 없을시 경비 한명 배치해야 함.
-                    x++;
-                }
-            }
-        }
-        // 행에 경비원이 없으면 경비 배치함.
-        // 열에 경비원이 없으면? 경비 배치함. 근데, 열 배치수 행 배치수 더 큰 거가 최솟값.
-        char [][] nrr = new char[m][n];
+        String [] dna = new String[n];
+        char [][] arr = new char[m][n];
         for(int i=0; i<n; i++){
-            for(int j=0;j<m;j++){
-                nrr[j][i] = arr[i].charAt(j);
+            dna[i] = bf.readLine();
+            for(int j=0; j<m; j++){
+                //각 열 별로 가장 많이 있는 것. 같은 수가 있으면 A넣기(사전순)
+                arr[j][i] = dna[i].charAt(j);
+
+                }
             }
+        String hdDNA = "";
+        for(int i=0;i<m;i++) {
+            int a = 0, c = 0, g = 0, t = 0;
+            int max = 0;
+            for (int j = 0; j < n; j++) {
+                char ch = arr[i][j];
+                switch (ch) {
+                    case 'A' -> a++;
+                    case 'C' -> c++;
+                    case 'G' -> g++;
+                    case 'T' -> t++;
+                }
+                if(a>max){ max=a;}
+                else if(c>max) {max=c;}
+                else if(g>max) {max=g;}
+                else if(t>max) {max=t;}
+
+            }
+            if(a==max){hdDNA += "A";}
+            else if(c==max){hdDNA += "C";}
+            else if(g==max){hdDNA += "G";}
+            else if(t==max){hdDNA += "T";}
+
         }
 
-        int y=0;
+        System.out.println(hdDNA);
+        int num = 0;
         for(int i=0; i<m; i++){
-            boolean status = false;
-
             for(int j=0; j<n; j++){
-                if (nrr[i][j]== 'X'){
-                    status = true; //경비 있으면 상태변경
-                }
-                if (status){
-                    break; // 경비 있으면 다음 행으로 넘어감
-                }
-                if(j==n-1){
-                    //열 끝까지 경비가 없을시 경비 한명 배치해야 함.
-                    y++;
+                if(arr[i][j] != hdDNA.charAt(i)) num++;
             }
-        }}
-        System.out.println(Math.max(x,y));
+        }
+        System.out.println(num);
 
     }
 }
